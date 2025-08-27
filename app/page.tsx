02e932ecfcff43224f623b10f460596b2e09c727
Kevini1 <cjdle.e5e@gmail.com>
2025-08-27 20:51:22 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Progress } from "@/components/ui/progress"
 import { Badge } from "@/components/ui/badge"
-import Script from 'next/script'
+import { Script } from 'next/script'
 import {
   Shield,
   ShieldAlert,
