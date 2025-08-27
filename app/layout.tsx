499c83d8f5cba3c0987bc7353297f08f7c101fa6
Kevini1 <cjdle.e5e@gmail.com>
2025-08-27 19:44:56 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'FastSecure.org - Protect Your Mobile Experience - fastsecure',
+  description: 'Protect Your Mobile Experience',
+  generator: 'fastsecure.org',
 }
 
 export default function RootLayout({
