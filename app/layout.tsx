ff0af93463a66644a44ce61231e1910cf5928f89
Kevini1 <cjdle.e5e@gmail.com>
2025-08-27 20:40:57 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,15 +16,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <!-- Google tag (gtag.js) -->
-      <script async='async' src='https://www.googletagmanager.com/gtag/js?id=G-21KY3C928N'></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-    
-        gtag('config', 'G-21KY3C928N');
-      </script> 
       <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
         {children}
       </body>
